3884131f1663708e5f5fbe22bc036914aefea53e
github username <github email address>
2018-08-20 12:13:41 -0400

    done

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,4 @@
 class Power < ApplicationRecord
+  has_many :heroines
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,15 @@ powers.each {|power| Power.create(power)}
 
 heroines = [
   {name: "Kamala Khan", super_name: "Ms. Marvel"},
-  {name: "Doreen Green", super_name: "Squirrel Girl" },
-  {name:"Gwen Stacy", super_name:"Spider-Gwen" },
-  {name:"Janet Van Dyne", super_name:"The Wasp" },
-  {name:"Wanda Maximoff", super_name:"Scarlet Witch" },
-  {name:"Carol Danvers", super_name:"Captain Marvel" },
-  {name:"Jean Grey", super_name:"Dark Phoenix" },
-  {name:"Ororo Munroe", super_name:"Storm" },
-  {name:"Shadowcat", super_name:"Kitty Pryde" },
-  {name:"Elektra", super_name:"Elektra Natchios" }
+  {name: "Doreen Green", super_name: "Squirrel Girl"},
+  {name:"Gwen Stacy", super_name:"Spider-Gwen"},
+  {name:"Janet Van Dyne", super_name:"The Wasp"},
+  {name:"Wanda Maximoff", super_name:"Scarlet Witch"},
+  {name:"Carol Danvers", super_name:"Captain Marvel"},
+  {name:"Jean Grey", super_name:"Dark Phoenix"},
+  {name:"Ororo Munroe", super_name:"Storm"},
+  {name:"Shadowcat", super_name:"Kitty Pryde"},
+  {name:"Elektra", super_name:"Elektra Natchios"}
 ]
 
 # ---------------
