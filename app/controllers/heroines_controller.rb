1631ed227cc4809e28e9adb3140b266a2d5f0bc5
github username <github email address>
2018-08-20 12:02:57 -0400

    donezo

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,54 @@
 class HeroinesController < ApplicationController
+  before_action :find_heroine, only: [:show, :edit, :update, :destroy]
+
   def index
-    @heroines = Heroine.all
+    if params[:superpower]
+      @heroines = Heroine.select {|heroine| heroine.power.name.include?(params[:superpower])}
+    else
+      @heroines = Heroine.all
+    end
+
+    #i think this is suppoed to be in models...NOT SURE!!
   end
-end
+
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  #show is implicit
+  #edit is implicit
+
+  def update
+    if @heroine.update(heroine_params)
+      redirect_to @heroine
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @heroine.destroy
+    redirect_to heroines_path
+  end
+
+  private
+
+  def find_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+end #end heroine class controller
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,15 @@
 class PowersController < ApplicationController
+  before_action :find_power, only: [:show]
+
   def index
     @powers = Power.all
+  end
+
+  #show is implicit
+
+  private
+
+  def find_power
+    @power = Power.find(params[:id])
   end
 end
