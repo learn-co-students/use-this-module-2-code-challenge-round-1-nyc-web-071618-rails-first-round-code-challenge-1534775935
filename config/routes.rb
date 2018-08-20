9540dfa7bcd1540adb7e73fe752e2c643f600f37
github username <github email address>
2018-08-20 11:41:12 -0400

    final mod 2 challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:show, :index]
+  resources :heroines, only: [:index, :show, :new, :create]
 end
