cb453ab2f7b79176d424a4eb8a94c43aa77cf25c
github username <github email address>
2018-08-20 11:39:44 -0400

    finished

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,17 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :super_name, uniqueness: true
+
+  def self.search(search)
+    if search
+      superpower = Power.find_by(name: search)
+      if superpower
+        self.where(power_id: superpower)
+      else
+        Heroine.all
+      end
+    else
+      Heroine.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:index, :new ,:create, :show, :edit, :update]
+  resources :heroines, only: [:index, :new, :create, :show, :edit, :update]
 end
