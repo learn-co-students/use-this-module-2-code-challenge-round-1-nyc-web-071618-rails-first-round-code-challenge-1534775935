027cc417fb5c0e5f538097146216cf03dbbf25a9
github username <github email address>
2018-08-20 11:51:28 -0400

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,39 @@
 class HeroinesController < ApplicationController
+
+
+
+
   def index
     @heroines = Heroine.all
+    # @heroines = @heroines.power(params[:power_id]) if params[:status].present?
+    # bonus
   end
-end
+
+  def show
+    @heroine=Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name,:super_name,:power_id)
+  end
+
+
+
+
+
+end # end of HeroinesController class
